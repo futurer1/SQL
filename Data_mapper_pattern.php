0868083ae43f1dc2d15a99ed9fb0aca1918ed37f
Michail <futurer@inbox.ru>
2017-06-06 23:41:39 +0400

    Update Data_mapper_pattern.php

--- Data_mapper_pattern.php
+++ Data_mapper_pattern.php
@@ -3,3 +3,31 @@
 * Шаблон Data Mapper
 * Управление передачей данных от БД к объекту
 */
+namespace woo\mapper;
+
+abstract class Mapper
+{
+    protected static $PDO; 
+  
+    public function __construct()
+    {
+        if ( ! isset(self::$PDO) ) { 
+            $dsn = \woo\base\ApplicationRegistry::getDSN( );
+            if ( is_null( $dsn ) ) {
+                throw new \woo\base\AppException( "DNS не определен" );
+            }
+            self::$PDO = new \PDO( $dsn );
+            self::$PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        }
+    }
+
+    public function find($id)
+    {
+    }
+
+    public function createObject( $array )
+    {
+    }
+
+    
+}

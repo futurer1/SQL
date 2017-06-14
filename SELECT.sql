2f776e04b68630994b4855e84e0dfc92a3d6cb06
Michail <futurer@inbox.ru>
2017-06-15 00:00:27 +0400

    Create SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,1 +1,7 @@
-SELECT * FROM table_name WHERE field1=1 ORDER BY field1 LIMIT 1
+SELECT `table_name1`.`field1`,
+        `table_name2`.`field1`
+FROM    `table_name1`, `table_name2` 
+WHERE   `table_name1`=NOW() AND 
+        (`table_name2`.`field1` BETWEEN 4 AND 6) AND 
+        `table_name2`.`field2` > 5
+ORDER BY `field1` DESC LIMIT 1

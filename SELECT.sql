5f0ebed5e5314c62d50f23d754ccac5c8bda329f
Michail <futurer@inbox.ru>
2017-07-11 20:17:49 +0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -25,3 +25,23 @@ SELECT COUNT(WCustomer=1) as countC,
 					`clients`.`WR` IS NOT NULL) AND 
 					`clients`.`ClientID`=`calls`.`ClientID` 
 		) as `tmp_table`
+
+SELECT `items`.`ItemID`, 
+	`items`.`MetatagsID`, 
+	`items`.`ManufacturerID`, 
+	`items`.`MeasureID`, 
+	`items`.`NameItem$lang` as `NameItem`, 
+	`items`.`AnnotItem$lang` as `AnnotItem`, 
+	`items`.`DescrItem$lang` as `DescrItem`, 
+	`items`.`ItemPrice`, 
+	`items`.`Status`, 
+	`index_items`.`FolderID`, 
+	`seo_metatags`.`TitleP$lang` as `TitleP`, 
+	`seo_metatags`.`DescriptionP$lang` as `DescriptionP`, 
+	`seo_metatags`.`RobotsP$lang` as `RobotsP`, 
+	`seo_metatags`.`KeywordsP$lang` as `KeywordsP` 
+FROM 	`index_items`, `items`, `seo_metatags` 
+WHERE 	`index_items`.`IndexID`=".$idi." AND 
+	`items`.`ItemID`=`index_items`.`ItemID` AND 
+	`seo_metatags`.`MetatagsID`=`items`.`MetatagsID` 
+LIMIT 1

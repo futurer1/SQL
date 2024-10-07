36c4d4ec1d752cdfcda5ef7d8872f43b8a7ff022
Mikhail <futurer@inbox.ru>
2024-10-07 11:47:24 +0300

    Create find-locks.sql

---
+++ PostgreSQL/find-locks.sql
@@ -0,0 +1,7 @@
+# Обнаружение локов в базе
+select pid,
+       usename,
+       pg_blocking_pids(pid) as blocked_by,
+       query as blocked_query
+from pg_stat_activity
+where cardinality(pg_blocking_pids(pid)) > 0;
